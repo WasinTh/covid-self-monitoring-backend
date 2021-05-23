9594e008d74df4406d5316032cb0c160686bd875
Terry <wasin.th@gmail.com>
2021-05-23 14:31:37 +0700

    Add swagger documentation

--- covid_self_monitoring/urls.py
+++ covid_self_monitoring/urls.py
@@ -1,8 +1,22 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework_simplejwt import views as jwt_views
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Covid Self Monitoring API",
+        default_version='v1',
+        description="bla bla..",
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+)
 
 urlpatterns = [
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0)),
     path('api-token-auth/', jwt_views.TokenObtainPairView.as_view()),
     path('api-token-refresh/', jwt_views.TokenRefreshView.as_view()),
     path('admin/', admin.site.urls),
