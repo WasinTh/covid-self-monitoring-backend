6a14bef9beda7f10d77a2fa98317dccd61b00913
Terry <wasin.th@gmail.com>
2021-05-23 11:37:00 +0700

    Add all-measurement API

--- monitor/urls.py
+++ monitor/urls.py
@@ -3,4 +3,5 @@ from monitor import views
 
 urlpatterns = [
     path('current-temperature/', views.current_temperature),
+    path('all-measurement/', views.all_measurement),
 ]
--- monitor/views.py
+++ monitor/views.py
@@ -6,3 +6,18 @@ from monitor.models import Measurement
 def current_temperature(request):
     data = {'temperature': str(Measurement.objects.last().temperature)}
     return HttpResponse(json.dumps(data))
+
+
+def all_measurement(request):
+    data = []
+    for m in Measurement.objects.all():
+        data.append({
+            'created': str(m.created),
+            'user': m.user.id,
+            'temperature': str(m.temperature),
+            'o2sat': m.o2sat,
+            'systolic': m.systolic,
+            'diastolic': m.diastolic,
+            'symptoms': m.symptoms_display,
+        })
+    return HttpResponse(json.dumps(data))
